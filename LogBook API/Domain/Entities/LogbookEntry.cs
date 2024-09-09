80d12919043d3eb80b21e7e4aeea862d5a94566b
Promise30 <promiseadeagbo@gmail.com>
2024-09-09 17:35:56 +0100

    initial project entity classes

---
+++ LogBook API/Domain/Entities/LogbookEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace LogBook_API.Domain.Entities
+{
+    public class LogbookEntry
+    {
+        public Guid Id { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string Activity { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+    }
+}
---
+++ LogBook API/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LogBook_API.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? PhoneCountryCode { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryDate { get; set; }
+        public virtual ICollection<LogbookEntry> LogbookEntries { get; set; } = new List<LogbookEntry>();
+    }
+}

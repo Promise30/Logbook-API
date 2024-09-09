0302f1cc2e682812313f45477648c8ffcb1e499f
Promise30 <promiseadeagbo@gmail.com>
2024-09-09 17:40:35 +0100

    Initial files for project query parameters specification

---
+++ LogBook API/Persistence/RequestParameters/DateFilterParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace LogBook_API.Persistence.RequestParameters
+{
+    public class DateFilterParameter
+    {
+        public DateOnly StartDate { get; set; } = DateOnly.MinValue;
+        public DateOnly EndDate { get; set; } = DateOnly.MaxValue;
+    }
+}

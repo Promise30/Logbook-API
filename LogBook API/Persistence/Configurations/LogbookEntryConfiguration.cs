540f5d1bdcb33cd6adc352aa672e7ae07eaec9c2
Promise30 <promiseadeagbo@gmail.com>
2024-09-09 17:36:26 +0100

    model configuration files with FluentApi

---
+++ LogBook API/Persistence/Configurations/LogbookEntryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using LogBook_API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace LogBook_API.Persistence.Configurations
+{
+    internal sealed class LogbookEntryConfiguration : IEntityTypeConfiguration<LogbookEntry>
+    {
+        public void Configure(EntityTypeBuilder<LogbookEntry> builder) 
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Activity)
+                    .HasMaxLength(50)
+                    .IsRequired();
+            builder.Property(e => e.Description)
+                   .HasMaxLength(200);
+            builder.Property(e => e.CreatedDate)
+                   .HasDefaultValueSql("GETDATE()");
+            builder.Property(e => e.LastUpdatedDate)
+                   .HasDefaultValueSql("GETDATE()");
+            builder.HasOne(e => e.User)
+                   .WithMany(e => e.LogbookEntries)
+                   .HasForeignKey(e => e.UserId);
+
+        }
+    }
+}
---
+++ LogBook API/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using LogBook_API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LogBook_API.Persistence.Configurations
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<User> 
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+            builder.Property(u => u.LastName)
+                   .HasMaxLength(50)
+                   .IsRequired();
+            builder.HasMany(u => u.LogbookEntries)
+                   .WithOne(l => l.User)
+                   .HasForeignKey(l => l.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+            builder.Property(u => u.CreatedDate)
+                  .HasDefaultValueSql("GETDATE()");
+            builder.Property(u => u.LastUpdatedDate)
+                   .HasDefaultValueSql("GETDATE()");
+
+        }
+    }
+}

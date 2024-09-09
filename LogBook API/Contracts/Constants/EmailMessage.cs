c1d490c5c29f04d81bd220ed797c37bd342b1671
Promise30 <promiseadeagbo@gmail.com>
2024-09-09 17:34:00 +0100

    initial email configuration related files

---
+++ LogBook API/Contracts/Constants/EmailMessage.cs
@@ -0,0 +1,21 @@
+﻿using MimeKit;
+
+namespace LogBook_API.Contracts.Constants
+{
+    public class EmailMessage
+    {
+            public List<MailboxAddress> To { get; set; }
+            public string Subject { get; set; }
+            public BodyBuilder Body { get; set; }
+            public EmailMessage(IEnumerable<string> to, string subject, string body)
+            {
+
+                To = new List<MailboxAddress>();
+                To.AddRange(to.Select(x => new MailboxAddress("email", x)));
+                Subject = subject;
+                Body = new BodyBuilder();
+                Body.HtmlBody = body;
+            }
+        
+    }
+}
---
+++ LogBook API/Contracts/Constants/EmailResponseMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace LogBook_API.Contracts.Constants
+{
+    public class EmailResponseMessage
+    {
+        public static string GetEmailSuccessMessage(string emailAddress) => $"Email sent successfully to {emailAddress}";   
+    }
+}

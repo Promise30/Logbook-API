415087fd2f5556a164097513d52b038af3f5c3f5
Promise30 <promiseadeagbo@gmail.com>
2024-09-09 17:34:44 +0100

    project related validation files for model validation

---
+++ LogBook API/Contracts/Validation/ValidRolesAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogBook_API.Contracts.Validation
+{
+    public class ValidRolesAttribute : ValidationAttribute
+    {
+        private readonly string[] _validRoles = { "Administrator", "User" };
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is ICollection<string> roles)
+            {
+                foreach (var role in roles)
+                {
+                    if (!_validRoles.Contains(role))
+                    {
+                        return new ValidationResult($"Invalid role: {role}. Valid roles are: {string.Join(", ", _validRoles)}");
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

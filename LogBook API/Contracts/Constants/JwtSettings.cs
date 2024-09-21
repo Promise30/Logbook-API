09df2136f15cd8998bd595ec061141cfd31468b1
Promise30 <promiseadeagbo@gmail.com>
2024-09-21 10:12:20 +0100

    Defined strongly typed classes for binding purpose to JWT config in .env file

---
+++ LogBook API/Contracts/Constants/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace LogBook_API.Contracts.Constants
+{
+    public class JwtSettings
+    {
+        public string validIssuer { get; set; }
+        public string validAudience { get; set; }
+        public string secretKey { get; set; }
+        public int expires { get; set; }
+    }
+}
